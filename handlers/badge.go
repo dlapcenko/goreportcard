719dc3a19c7365105086351cd12234968ba30ce1
dlapcenko <dlapcenko@salmon.com>
2018-06-21 15:44:13 +0300

    CLOUD:818-819: Removed all web-app code

--- handlers/badge.go
+++ handlers/badge.go
@@ -2,8 +2,6 @@ package handlers
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 	"strings"
 )
 
@@ -45,25 +43,4 @@ func grade(percentage float64) Grade {
 
 func badgePath(grade Grade, style string, dev bool) string {
 	return fmt.Sprintf("assets/badges/%s_%s.svg", strings.ToLower(string(grade)), strings.ToLower(style))
-}
-
-// BadgeHandler handles fetching the badge images
-func BadgeHandler(w http.ResponseWriter, r *http.Request, repo string, dev bool) {
-	resp, err := newChecksResp(repo, false)
-
-	// See: http://shields.io/#styles
-	style := r.URL.Query().Get("style")
-	if style == "" {
-		style = "flat"
-	}
-
-	if err != nil {
-		log.Printf("ERROR: fetching badge for %s: %v", repo, err)
-		url := "https://img.shields.io/badge/go%20report-error-lightgrey.svg?style=" + style
-		http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-		return
-	}
-
-	w.Header().Set("Cache-control", "no-store, no-cache, must-revalidate")
-	http.ServeFile(w, r, badgePath(resp.Grade, style, dev))
 }
--- handlers/checks.go
+++ handlers/checks.go
@@ -1,14 +1,11 @@
 package handlers
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"sort"
 	"time"
 
-	"github.com/boltdb/bolt"
 	"github.com/dustin/go-humanize"
 	"github.com/gojp/goreportcard/check"
 	"github.com/gojp/goreportcard/download"
@@ -25,43 +22,6 @@ func (n notFoundError) Error() string {
 
 func dirName(repo string) string {
 	return fmt.Sprintf("_repos/src/%s", repo)
-}
-
-func getFromCache(repo string) (checksResp, error) {
-	// try and fetch from boltdb
-	db, err := bolt.Open(DBPath, 0600, &bolt.Options{Timeout: 3 * time.Second})
-	if err != nil {
-		return checksResp{}, fmt.Errorf("failed to open bolt database during GET: %v", err)
-	}
-	defer db.Close()
-
-	resp := checksResp{}
-	err = db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(RepoBucket))
-		if b == nil {
-			return errors.New("No repo bucket")
-		}
-		cached := b.Get([]byte(repo))
-		if cached == nil {
-			return notFoundError{repo}
-		}
-
-		err = json.Unmarshal(cached, &resp)
-		if err != nil {
-			return fmt.Errorf("failed to parse JSON for %q in cache", repo)
-		}
-		return nil
-	})
-
-	if err != nil {
-		return resp, err
-	}
-
-	resp.LastRefresh = resp.LastRefresh.UTC()
-	resp.LastRefreshFormatted = resp.LastRefresh.Format(time.UnixDate)
-	resp.LastRefreshHumanized = humanize.Time(resp.LastRefresh.UTC())
-
-	return resp, nil
 }
 
 type score struct {
@@ -101,94 +61,6 @@ func newChecksRespPrivate(repo string) (checksResp, error) {
 	resp, err := checkResp(dir)
 	resp.Repo = repo //allows abstracting checkResp for local and remote checks, yet leaving code largely same
 	resp.ResolvedRepo = repoRoot.Root
-
-	return resp, nil
-}
-
-func newChecksResp(repo string, forceRefresh bool) (checksResp, error) {
-	if !forceRefresh {
-		resp, err := getFromCache(repo)
-		if err != nil {
-			// just log the error and continue
-			log.Println(err)
-		} else {
-			resp.Grade = grade(resp.Average * 100) // grade is not stored for some repos, yet
-			return resp, nil
-		}
-	}
-
-	// fetch the repo and grade it
-	repoRoot, err := download.Download(repo, "_repos/src")
-	if err != nil {
-		return checksResp{}, fmt.Errorf("could not clone repo: %v", err)
-	}
-
-	repo = repoRoot.Root
-
-	dir := dirName(repo)
-	resp, err := checkResp(dir)
-	resp.Repo = repo //allows abstracting checkResp for local and remote checks, yet leaving code largely same
-	resp.ResolvedRepo = repoRoot.Root
-
-	respBytes, err := json.Marshal(resp)
-	if err != nil {
-		return checksResp{}, fmt.Errorf("could not marshal json: %v", err)
-	}
-
-	// write to boltdb
-	db, err := bolt.Open(DBPath, 0755, &bolt.Options{Timeout: 1 * time.Second})
-	if err != nil {
-		return checksResp{}, fmt.Errorf("could not open bolt db: %v", err)
-	}
-	defer db.Close()
-
-	// is this a new repo? if so, increase the count in the high scores bucket later
-	isNewRepo := false
-	var oldRepoBytes []byte
-	err = db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(RepoBucket))
-		if b == nil {
-			return fmt.Errorf("repo bucket not found")
-		}
-		oldRepoBytes = b.Get([]byte(repo))
-		return nil
-	})
-	if err != nil {
-		log.Println("ERROR getting repo from repo bucket:", err)
-	}
-
-	isNewRepo = oldRepoBytes == nil
-
-	// if this is a new repo, or the user force-refreshed, update the cache
-	if isNewRepo || forceRefresh {
-		err = db.Update(func(tx *bolt.Tx) error {
-			log.Printf("Saving repo %q to cache...", repo)
-
-			b := tx.Bucket([]byte(RepoBucket))
-			if b == nil {
-				return fmt.Errorf("repo bucket not found")
-			}
-
-			// save repo to cache
-			err = b.Put([]byte(repo), respBytes)
-			if err != nil {
-				return err
-			}
-
-			return updateMetadata(tx, resp, repo, isNewRepo)
-		})
-
-		if err != nil {
-			log.Println("Bolt writing error:", err)
-		}
-
-	}
-
-	db.Update(func(tx *bolt.Tx) error {
-		// fetch meta-bucket
-		mb := tx.Bucket([]byte(MetaBucket))
-		return updateRecentlyViewed(mb, repo)
-	})
 
 	return resp, nil
 }
